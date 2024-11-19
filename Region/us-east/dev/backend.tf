495d009f2884edf4ca34d7e0fca912c0e0e83fb5
phungh67 <106119538+phungh67@users.noreply.github.com>
2024-11-20 00:15:18 +0700

    feat: update data for student

--- Region/us-east/dev/backend.tf
+++ Region/us-east/dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
-	backend "s3" {
-		bucket         	   = "deu1-state-bucket"
-    		key              	   = "state/terraform.tfstate"
-    		region         	   = "us-east-1"
-    		encrypt        	   = true
-	}
+  backend "s3" {
+    bucket  = "deu1-state-bucket"
+    key     = "state/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
 }
